bce4488b934da6fd7eeefeb7c5d65562a201f452
Brian K. Vagnini <bkvagnini@gmail.com>
2020-04-19 15:44:02 -0400

    Make changes to Mod2.

---
+++ Mod2/FoodieGuyz/js/home.js
@@ -0,0 +1,3 @@
+  // document.write("Our Chef's Daily special is: ");
+
+  document.getElementById('special').innerHTML = "<h3>Our Chef's Daily special is: </h3> <p>Meatloaf with Garlic Smashed Potatoes.</p>";
